0a36afe33462035778ad9c888296a984ed6a3f35
menoudste <stephane.menoud26@gmail.com>
2013-10-01 15:23:59 +0200

    Update nombre-de-jours-dans-le-mois-validation.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/nombre-de-jours-dans-le-mois-validation.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/nombre-de-jours-dans-le-mois-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier nombre-de-jours-dans-le-mois-validation.py
 # nom de l'exercice :  Nombre de jours dans le mois
@@ -17,3 +16,25 @@
 
 # mettre votre code ici
 
+M1 = 30
+M2 = 30
+M3 = 30
+M4 = 31
+M5 = 31
+M6 = 31
+M7 = 30
+M8 = 30
+M9 = 30
+M10 = 31
+M11 = 29
+
+M = int(input())
+
+if (M >= 1 and M <=3) or (M >= 7 and M <= 9):
+   print("30")
+
+if (M >= 4 and M <=6) or (M == 10):
+   print("31")
+   
+if (M ==11):
+   print("29")
