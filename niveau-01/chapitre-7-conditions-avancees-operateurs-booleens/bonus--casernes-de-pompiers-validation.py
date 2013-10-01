3186d6da7d57f38faa2826edc1aab90da81abdcf
menoudste <stephane.menoud26@gmail.com>
2013-10-01 15:25:22 +0200

    Update bonus--casernes-de-pompiers-validation.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/bonus--casernes-de-pompiers-validation.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/bonus--casernes-de-pompiers-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier bonus--casernes-de-pompiers-validation.py
 # nom de l'exercice :  Bonus : Casernes de pompiers
@@ -17,3 +16,30 @@
 
 # mettre votre code ici
 
+for loop in range(int(input())):
+   xmin1 = int(input())
+   xmax1 = int(input())
+   ymin1 = int(input())
+   ymax1 = int(input())
+   
+   xmin2 = int(input())
+   xmax2 = int(input())
+   ymin2 = int(input())
+   ymax2 = int(input())
+   
+if (xmax1 <= xmin2 or xmax2 <= xmin1) or (ymax1 <= ymin2 or ymax2 <= ymin1):
+   print("NON")
+
+else:
+   print("OUI")
+   
+
+
+
+
+   
+   
+
+   
+   
+   
