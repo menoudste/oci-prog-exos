15686cda179a9fa4f85eb05b6f5df8a064b9bac5
menoudste <stephane.menoud26@gmail.com>
2013-10-01 15:27:23 +0200

    Update l-espion-est-demasque--validation.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/l-espion-est-demasque--validation.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/l-espion-est-demasque--validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier l-espion-est-demasque--validation.py
 # nom de l'exercice :  L'espion est démasqué !
@@ -17,3 +16,35 @@
 
 # mettre votre code ici
 
+criteres = 0
+nbPersonnes = int(input())
+
+for loop in range(nbPersonnes):
+   taille = int(input())
+   age  = int(input())
+   poids  = int(input())
+   cheval = int(input())
+   cheveux = int(input())
+   
+   if taille > 178 and taille < 182:
+      criteres = criteres + 1
+   if age >= 34:
+      criteres = criteres + 1
+   if poids < 70:
+      criteres = criteres + 1
+   if cheval == 0:
+      criteres = criteres + 1
+   if cheveux == 1:
+      criteres = criteres + 1
+
+   if criteres == 5:
+      print("Très probable")
+   elif criteres == 4 or criteres == 3:
+      print("Probable")
+   elif criteres == 0:
+      print("Impossible")
+   else:
+      print("Peu probable")
+   
+      
+         
