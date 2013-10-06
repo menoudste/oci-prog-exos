6499ba52bd1f67515c0c022359d8899f4bccf967
menoudste <stephane.menoud26@gmail.com>
2013-10-07 00:01:19 +0200

    Update departement-de-pedagogie--le-c-est-plus-c-est-moins--entrainement.py

--- niveau-01/chapitre-8-repetitions-tant-que/departement-de-pedagogie--le-c-est-plus-c-est-moins--entrainement.py
+++ niveau-01/chapitre-8-repetitions-tant-que/departement-de-pedagogie--le-c-est-plus-c-est-moins--entrainement.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier departement-de-pedagogie--le-c-est-plus-c-est-moins--entrainement.py
 # nom de l'exercice :  Département de pédagogie : le c'est plus, c'est moins !
@@ -17,3 +16,22 @@
 
 # mettre votre code ici
 
+nombre = int(input())
+prop = 0
+essais = 1
+
+while prop != nombre:
+   prop = int(input())
+   if prop > nombre:
+      print("c'est moins")
+      essais = essais + 1
+   
+   if prop < nombre:
+      print("c'est plus")
+      essais = essais + 1
+
+
+
+print("Nombre d'essais nécessaires :")
+print(essais)
+      
