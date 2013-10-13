84300001eb562b33c9faf5215f6550a20c77ee0a
menoudste <stephane.menoud26@gmail.com>
2013-10-13 10:45:57 +0200

    Update departement-d-architecture--construction-d-une-pyramide-validation.py

--- niveau-01/chapitre-8-repetitions-tant-que/departement-d-architecture--construction-d-une-pyramide-validation.py
+++ niveau-01/chapitre-8-repetitions-tant-que/departement-d-architecture--construction-d-une-pyramide-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier departement-d-architecture--construction-d-une-pyramide-validation.py
 # nom de l'exercice :  Département d'architecture : construction d'une pyramide
@@ -16,4 +15,31 @@
 
 
 # mettre votre code ici
+
+nbPierresDispo = int(input())
+nbPierres = 0
+nbEtages = 1
+total = 0
+cote = 1
+
+
+while nbPierresDispo > total :
+   nbPierres = cote**2
+   cote = cote + 1
+   nbEtages = nbEtages + 1
+   total = total + nbPierres
+   
+if nbPierresDispo > 1:   
+    total = total - ((cote-1)**2)#Sinon, un étage de pierres en trop est comptabilisé
+
+    print(nbEtages - 2) #Sinon, il y aurait 2 étages de trop
+    print(total)
+
+if nbPierresDispo == 1 :
+    print("1")
+    print("1")
+
+if nbPierresDispo <= 0 :
+    print("0")
+    print("0")
 
