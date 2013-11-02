bd5fa725d3ecfb99248200ce11c1455c25d7dbba
menoudste <stephane.menoud26@gmail.com>
2013-11-02 10:04:21 +0100

    Update 15-soiree-orageuse-obligatoire.py

--- niveau-02/chapitre-1-nombres-virgules-autres-outils/15-soiree-orageuse-obligatoire.py
+++ niveau-02/chapitre-1-nombres-virgules-autres-outils/15-soiree-orageuse-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 15-soiree-orageuse-obligatoire.py
 # nom de l'exercice : Soirée orageuse
@@ -17,3 +16,12 @@
 
 # mettre votre code ici
 
+from math import *
+distance = 0
+
+temps = float(input())
+
+
+distance = round(temps * 0.34029)
+
+print(distance)
