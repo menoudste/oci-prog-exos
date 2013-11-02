07bb87d171190e648f1973f2c0d4301410319b15
menoudste <stephane.menoud26@gmail.com>
2013-11-02 10:01:53 +0100

    Update 04-conversions-de-distances-obligatoire.py

--- niveau-02/chapitre-1-nombres-virgules-autres-outils/04-conversions-de-distances-obligatoire.py
+++ niveau-02/chapitre-1-nombres-virgules-autres-outils/04-conversions-de-distances-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 04-conversions-de-distances-obligatoire.py
 # nom de l'exercice : Conversions de distances
@@ -17,3 +16,6 @@
 
 # mettre votre code ici
 
+nbLieues = float(input())
+
+print(nbLieues * 1.414427157) 
