2d07be84e2420f45964d1abb85ec7e2c800efdd6
menoudste <stephane.menoud26@gmail.com>
2013-12-09 21:58:16 +0100

    Update 02g-resumes-de-livres-obligatoire.py

--- niveau-02/chapitre-3-chaines-de-caracteres/02g-resumes-de-livres-obligatoire.py
+++ niveau-02/chapitre-3-chaines-de-caracteres/02g-resumes-de-livres-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 02g-resumes-de-livres-obligatoire.py
 # nom de l'exercice : Résumés de livres
@@ -17,3 +16,13 @@
 
 # mettre votre code ici
 
+nbLivres = int(input())
+longueurMin = int(input())
+
+for loop in range (nbLivres):
+   titre = input()
+   resume = input()
+   
+   if len(resume) < longueurMin :
+      print(titre)
+   
