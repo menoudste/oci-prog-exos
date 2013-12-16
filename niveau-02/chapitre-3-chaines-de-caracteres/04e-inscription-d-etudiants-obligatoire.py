f877d54c24adc0301498baf5a391c9677df4edfb
menoudste <stephane.menoud26@gmail.com>
2013-12-16 21:52:05 +0100

    Update 04e-inscription-d-etudiants-obligatoire.py

--- niveau-02/chapitre-3-chaines-de-caracteres/04e-inscription-d-etudiants-obligatoire.py
+++ niveau-02/chapitre-3-chaines-de-caracteres/04e-inscription-d-etudiants-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 04e-inscription-d-etudiants-obligatoire.py
 # nom de l'exercice : Inscription d’étudiants
@@ -17,3 +16,25 @@
 
 # mettre votre code ici
 
+nom = input()
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lettre = 0
+
+while alphabet[lettre] != nom[0]:
+
+   if alphabet[lettre] == nom[0]:
+      idLettre = lettre
+
+   lettre += 1
+
+
+lettre -= 1 #Sinon, "lettre" a été incrémenté une fois de trop.
+
+if lettre <=5 :
+   print(1)
+   
+elif lettre <= 15 :
+   print(2)
+   
+else :
+   print(3)
