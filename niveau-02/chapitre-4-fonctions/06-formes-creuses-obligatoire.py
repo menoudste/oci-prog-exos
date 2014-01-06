682240ae04a8d8b61eb3905b323ffc9711b4b81c
menoudste <stephane.menoud26@gmail.com>
2014-01-06 19:58:28 +0100

    Update 06-formes-creuses-obligatoire.py

--- niveau-02/chapitre-4-fonctions/06-formes-creuses-obligatoire.py
+++ niveau-02/chapitre-4-fonctions/06-formes-creuses-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 06-formes-creuses-obligatoire.py
 # nom de l'exercice : Formes creuses
@@ -17,3 +16,47 @@
 
 # mettre votre code ici
 
+def forme(ligne, lignesRectangle, colonnesRectangle, lignesTri):
+   
+   # Ligne
+   for loop in range(ligne):
+      print("X", end = "")
+   
+   print()
+   print()
+   
+   #Rectangle creux
+   for loop in range(colonnesRectangle):
+      print("#", end = "")
+   
+   print()
+   
+   # Dans le cas où il y aurait plus qu'une seule colonne
+   if colonnesRectangle > 1:
+      for loop in range(lignesRectangle - 2):
+         print("#" + " "*(colonnesRectangle - 2) + "#",)
+     
+      for loop in range(colonnesRectangle):
+         print("#", end = "")
+   
+   # Dans le cas où il n'y aurait qu'une seule colonne
+   if colonnesRectangle == 1:
+   
+      for loop in range(lignesRectangle - 1): # Sinon il y un "#" de trop, la première ligne est déjà rajoutée plus haut
+         print("#")
+         
+   
+   print()
+   print()
+   
+   #Triangle
+   print("@")
+   
+   for nbRepetitions in range(lignesTri - 2):
+      print("@" + " " * nbRepetitions + "@")
+   
+   for loop in range(lignesTri):
+      print("@", end = "")
+      
+
+forme(int(input()), int(input()), int(input()), int(input()))
