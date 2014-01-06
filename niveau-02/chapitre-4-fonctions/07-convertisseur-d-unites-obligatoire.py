73bd9485bd31c9f14d4440e801ebb2387e892f7f
menoudste <stephane.menoud26@gmail.com>
2014-01-06 19:58:55 +0100

    Update 07-convertisseur-d-unites-obligatoire.py

--- niveau-02/chapitre-4-fonctions/07-convertisseur-d-unites-obligatoire.py
+++ niveau-02/chapitre-4-fonctions/07-convertisseur-d-unites-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 07-convertisseur-d-unites-obligatoire.py
 # nom de l'exercice : Convertisseur d'unités
@@ -17,3 +16,45 @@
 
 # mettre votre code ici
 
+N = int(input())
+
+def conversion():
+   
+   for caractere in range(N):
+      valeur = input()
+      
+      if valeur[-1] == 'm':
+         valeur = valeur[:-1]
+         valeur = float(valeur)
+         multiplication = 1 / 0.3048
+         valeur *= multiplication
+         valeur = round(valeur, 6)
+         valeur = str(valeur)
+         print(valeur + " p")
+      
+      if valeur[-1] == 'g':
+         valeur = valeur[:-1]
+         valeur = float(valeur)
+         multiplication = 0.002205 / 1
+         valeur *= multiplication
+         valeur = round(valeur, 6)
+         valeur = str(valeur)
+         print(valeur + " l")
+      
+      if valeur[-1] == 'c':
+         valeur = valeur[:-1]
+         valeur = float(valeur)
+         valeur = 32+(1.8 * valeur)
+         valeur = round(valeur, 6)
+         valeur = str(valeur)
+         print(valeur + " f")
+      
+   return valeur
+   
+   
+         
+         
+         
+        
+         
+conversion()
