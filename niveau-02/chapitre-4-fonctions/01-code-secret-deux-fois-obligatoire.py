bbb778f6fd5ede6e4d6f807fb1a0c6de9232844b
menoudste <stephane.menoud26@gmail.com>
2014-01-06 19:56:44 +0100

    Update 01-code-secret-deux-fois-obligatoire.py

--- niveau-02/chapitre-4-fonctions/01-code-secret-deux-fois-obligatoire.py
+++ niveau-02/chapitre-4-fonctions/01-code-secret-deux-fois-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 01-code-secret-deux-fois-obligatoire.py
 # nom de l'exercice : Code secret deux fois
@@ -17,3 +16,25 @@
 
 # mettre votre code ici
 
+script = 0
+
+def code():
+
+      print("Entrez le code :")
+      
+      script = int(input())
+      
+      while script != 4242 :
+         script = int(input())
+         print("Entrez le code :")
+      
+      
+      return script
+      
+code()
+
+print("Encore une fois.")
+
+code()
+
+print("Bravo.")
